170385e755583ba8cef4486e5f8ce8179ad425a5
Sayan1000 <sayanmondal0202@gmail.com>
2025-12-13 12:35:07 +0530

    second

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home(){
         <VideoGallery items={[
           { id:'v1', src:'/media/2.mp4', thumb:'/media/Thumbnail2.jpg', caption:'Denture finishing'},
           { id:'v2', src:'/media/video2.mp4', thumb:'/media/Thumbnail1.webp', caption:'Polishing workflow'},
-          { id:'v3', src:'/media/video3.mp4', thumb:'/media/Thumbnail3.webp', caption:'Clinical Explanation by the Dentist'}
+          { id:'v3', src:'/media/Video3.mp4', thumb:'/media/Thumbnail3.webp', caption:'Clinical Explanation by the Dentist'}
         ]} />
         <Testimonials />
         <ContactBlock />
